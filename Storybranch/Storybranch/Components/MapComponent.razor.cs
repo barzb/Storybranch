9a979bc8334de8cda8a311561e8728d3aa9e25b9
Benni <barzbenni@gmail.com>
2021-10-17 20:11:50 +0200

    finished cleaning up MapComponentBase

--- Storybranch/Storybranch/Components/MapComponent.razor.cs
+++ Storybranch/Storybranch/Components/MapComponent.razor.cs
@@ -1,4 +1,5 @@
-﻿using System.Drawing;
+﻿using System.Collections.Generic;
+using System.Drawing;
 using BlazorLeaflet;
 using BlazorLeaflet.Models;
 using Microsoft.AspNetCore.Components;
@@ -16,8 +17,11 @@ namespace Storybranch.Components
 		// --- Injects ---
 		[Inject] protected IJSRuntime JsRuntime { get; set; }
 
-		// --- Properties ---
+		// --- Protected Properties ---
 		protected Map Map { get; set; }
+
+		// --- Private Properties ---
+		private List<Layer> MapLayers { get; set; } = new List<Layer>();
 		private Circle Circle { get; set; }
 		private LatLng StartAt { get; set; } = new LatLng(47.5574007f, 16.3918687f);
 
@@ -31,51 +35,71 @@ namespace Storybranch.Components
 				MaxZoom = MaxZoom,
 				MinZoom = MinZoom
 			};
+			CreateMapLayers();
 
-			Map.OnInitialized += () =>
-			{
-				Map.AddLayer(new TileLayer
-				{
-					UrlTemplate = "http://battosai.de/jedaya/map/tiles/{z}/{x}/{y}.jpg",
-					Attribution = "...",
-				});
-
-				Map.AddLayer(new Polygon
-				{
-					Shape = new[]
-					{
-						new[]
-						{
-							new PointF(37f, -109.05f), new PointF(41f, -109.03f), new PointF(41f, -102.05f),
-							new PointF(37f, -102.04f)
-						}
-					},
-					Fill = true,
-					FillColor = Color.Blue,
-					Popup = new Popup
-					{
-						Content = "How are you doing,"
-					}
-				});
-
-				Circle = new Circle
-				{
-					Position = new LatLng(10f, 5f),
-					Radius = 10f
-				};
-				Map.AddLayer(Circle);
-			};
+			Map.OnInitialized += SetupLeafletMap;
 		}
 
-		// --- Methods ---
-		private void ZoomMap()
+		// --- Public Methods ---
+
+		// --- Protected Methods ---
+		protected void TestZoomMap()
 		{
 			Map.FitBounds(new PointF(45.943f, 24.967f), new PointF(46.943f, 25.967f), maxZoom: 5f);
 		}
 
-		private void PanToSomewhere()
+		protected void TestPanToSomewhere()
 		{
 			Map.PanTo(new PointF(40.713185f, -74.0072333f), animate: true, duration: 10f);
+		}
+
+		// --- Private Methods ---
+		private void CreateMapLayers()
+		{
+			// Tile layers.
+			TileLayer backgroundTileLayer = new TileLayer()
+			{
+				UrlTemplate = "http://battosai.de/jedaya/map/tiles/{z}/{x}/{y}.jpg",
+				Attribution = "...",
+			};
+
+			// Shape layers.
+			Polygon polygonTestLayer = new Polygon()
+			{
+				Shape = new[]
+				{
+					new[]
+					{
+						new PointF(37f, -109.05f), new PointF(41f, -109.03f), new PointF(41f, -102.05f),
+						new PointF(37f, -102.04f)
+					}
+				},
+				Fill = true,
+				FillColor = Color.Blue,
+				Popup = new Popup
+				{
+					Content = "How are you doing,"
+				}
+			};
+			Circle circleTestLayer = new Circle
+			{
+				Position = new LatLng(10f, 5f),
+				Radius = 10f
+			};
+
+			// Add to list, the order is important!
+			MapLayers.Add(backgroundTileLayer);
+			MapLayers.Add(polygonTestLayer);
+			MapLayers.Add(circleTestLayer);
+		}
+
+		private void SetupLeafletMap()
+		{
+			// Add layers to map.
+			foreach (var layerToAdd in MapLayers)
+			{
+				Map.AddLayer(layerToAdd);
+			}
 		}
 	}
 }
