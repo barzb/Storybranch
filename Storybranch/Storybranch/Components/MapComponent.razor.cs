708f0f34cd0ab265b0e383b05ec58d4b270d989a
Benni <barzbenni@gmail.com>
2021-10-17 18:53:07 +0200

    added LeafletBlazor to project and did some experiments

---
+++ Storybranch/Storybranch/Components/MapComponent.razor.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Drawing;
+using System.Threading.Tasks;
+using BlazorLeaflet;
+using BlazorLeaflet.Models;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace Storybranch.Components
+{
+	public class MapComponentBase : ComponentBase
+	{
+		[Inject]
+		protected IJSRuntime JsRuntime { get; set; }
+
+		[Parameter]
+		public string MapName { get; set; } = "map";
+
+		protected Map _map;
+		private PointF _startAt = new PointF(47.5574007f, 16.3918687f);
+
+		protected override void OnInitialized()
+		{
+			/*_map = new Map(JsRuntime);
+			_map.Layers.Add(new TileLayer
+			{
+				UrlTemplate = "http://battosai.de/jedaya/map/tiles/{z}/{x}/{y}.jpg",
+				Attribution = "",
+			});*/
+		}
+
+		protected void InitMap()
+		{
+			//Console.WriteLine("initLeaflet");
+			//JsRuntime.InvokeVoidAsync("initLeaflet", MapName);
+			//StateHasChanged();
+		}
+	}
+}
---
+++ Storybranch/Storybranch/wwwroot/js/LeafletController.js
@@ -0,0 +1,35 @@
+﻿function initLeaflet(divTargetName)
+{
+    var mapMinZoom = 2;
+    var mapMaxZoom = 12;
+    var mapMaxResolution = 1.00000000;
+    var mapMinResolution = Math.pow(2, mapMaxZoom) * mapMaxResolution;
+    var tileExtent = [0.00000000, -7680.00000000, 4320.00000000, 0.00000000];
+    var crs = L.CRS.Simple;
+    crs.transformation = new L.Transformation(1, -tileExtent[0], -1, tileExtent[3]);
+    crs.scale = function (zoom) {
+        return Math.pow(2, zoom) / mapMinResolution;
+    };
+    crs.zoom = function (scale) {
+        return Math.log(scale * mapMinResolution) / Math.LN2;
+    };
+    var map = new L.Map(divTargetName, {
+        maxZoom: mapMaxZoom,
+        minZoom: mapMinZoom,
+        crs: crs
+    });
+
+    var layer = L.tileLayer('http://battosai.de/jedaya/map/tiles/{z}/{x}/{y}.jpg', {
+        minZoom: mapMinZoom,
+        maxZoom: mapMaxZoom,
+        attribution: 'Kangaroos are great',
+        noWrap: true,
+        tms: false
+    }).addTo(map);
+
+    map.setView(
+        [-535000, 500000],
+        3
+    );
+    L.control.mousePosition().addTo(map);
+}
